dfd515323583dd7bdd85d6fea29476a6205929b8
SoumenCodes <soumen19j@gmail.com>
2024-02-10 18:47:23 +0530

    some proxy

--- src/components/api/config.js
+++ src/components/api/config.js
@@ -1,4 +1,5 @@
 // config.js
-const API_URL = "http://localhost:3000"; // Change this to your API URL
+// const API_URL = "http://localhost:3000"; // Change this to your API URL
+const API_URL = "https://woodata.onrender.com"; // Change this to your API URL
 
 export default API_URL;
